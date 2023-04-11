c30bd7f6aeb190be951e927184c2ab572b1d2d66
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-10 22:31:28 -0700

    add querySelector event listeners to hide and show Actions menu items.

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+
+
+const menu = document.querySelector('.menu')
+const menuItems = menu.querySelector('.items')
+
+menu.addEventListener('click', event => {
+  menuItems.classList.toggle('hidden');
+});
