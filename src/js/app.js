d2f3d4b99b59fec09d5eba95327028865e4316cd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-11 18:17:16 -0700

    Add eventListener for squares querySelector and iterated through nodelist using forEach. with temporary console.log()

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@
 const App = {
   // All of our selected HTML elements
   $: {
-    
     menu: document.querySelector('[data-id="menu"]'),
     menuItems: document.querySelector('[data-id="menu-items"]'),
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
@@ -12,9 +11,6 @@ const App = {
 
   // Where we will add eventListeners to our application
   init(){
-
-    console.log(App.$.squares)
-
     App.$.menu.addEventListener('click', event => {
       App.$.menuItems.classList.toggle('hidden');
     });
@@ -26,6 +22,12 @@ const App = {
     App.$.newRoundBtn.addEventListener('click', event => {
       console.log("Add a new round")
     });
+
+    App.$.squares.forEach(square => {
+      square.addEventListener('click', event => {
+        console.log(`Square w/ id ${event.target.id} was clicked`)
+      })
+    })
   },
 }
 
