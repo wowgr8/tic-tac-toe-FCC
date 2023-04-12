d1049868ba61f142251420f90e4214ba7c183dc3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-11 19:06:08 -0700

    remove console logs and add comments. Add winningPatterns array - WIP

--- src/js/app.js
+++ src/js/app.js
@@ -33,13 +33,13 @@ const App = {
 
     App.$.squares.forEach(square => {
       square.addEventListener('click', event => {
-        console.log(`Square w/ id ${event.target.id} was clicked`);
-        console.log(`Current player is: ${App.state.currentPlayer}`);
 
+        // Check if there is already a play, if so, return early.
         if (square.hasChildNodes()) {
           return
         }
 
+        // Determine which player icon to add to the square.
         const currentPlayer = App.state.currentPlayer
         const icon = document.createElement('i');
 
@@ -52,6 +52,19 @@ const App = {
         App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
 
         square.replaceChildren(icon);
+
+        // Check if there is a winner or tie game
+        const winningPatterns = [
+          [1, 2, 3],
+          [1, 5, 9],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 5, 7],
+          [3, 6, 9],
+          [4, 5, 6],
+          [7, 8, 9],
+        ];
+
       })
     });
   }
