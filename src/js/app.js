b95a2535a93f1130679dcb4ca531e22c602a2786
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-11 18:02:55 -0700

    add namespaces for reset-button, and new round button. Add data-id's in querySelector

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,18 @@ const App = {
   // All of our selected HTML elements
   $: {
     
-    menu: document.querySelector('.menu'),
-    menuItems: document.querySelector('.items'),
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
   },
   // Where we will add eventListeners to our application
   init(){
     App.$.menu.addEventListener('click', event => {
       App.$.menuItems.classList.toggle('hidden');
     });
+
+    
   },
 }
 
